057fba54d6aa16e740db29bb6977a276203e00b2
StopSkiddingThis <112759165+StopSkiddingThis@users.noreply.github.com>
2022-09-03 13:13:46 +0100

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -107,7 +107,7 @@ if isfolder("vape/assets") == false then
 	makefolder("vape/assets")
 end
 
-local GuiLibrary = loadstring(GetURL("NewGuiLibrary.lua"))()
+local GuiLibrary = loadstring(game:HttpGet('https://raw.githubusercontent.com/StopSkiddingThis/PythonCode/main/NewGuiLibrary.lua', true))()
 local translations = {}--loadstring(GetURL("translations/"..GuiLibrary["Language"]..".vapetranslation"))()
 local translatedlogo = false--pcall(function() return GetURL("translations/"..GuiLibrary["Language"].."/VapeLogo1.png") end)
 
